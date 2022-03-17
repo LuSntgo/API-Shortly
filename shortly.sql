9d8bbcbced97e73a8c41ff135b86fc91ba21f206
Luiza Santiago <lu.sntgo@gmail.com>
2022-03-17 19:24:58 -0300

    feat get urls

---
+++ shortly.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE sessions (
+   id SERIAL PRIMARY KEY,
+   token TEXT NOT NULL UNIQUE,
+   "userId" INTEGER NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE urls (
+   id SERIAL PRIMARY KEY,
+   "shortUrl" TEXT NOT NULL UNIQUE,
+   url TEXT NOT NULL
+);
+
+CREATE TABLE usersUrls (
+   id SERIAL PRIMARY KEY,
+   "userId" INTEGER NOT NULL REFERENCES users(id),
+   "urlsId" INTEGER NOT NULL REFERENCES urls(id)
+);
--- src/database.js
+++ src/database.js
@@ -6,3 +6,19 @@ const { Pool } = pg;
 export const connection = new Pool({
   connectionString: process.env.DATABASE_URL
 });
+
+
+// CREATE TABLE shortUrls(
+//   id SERIAL PRIMARY KEY,
+//   "shortUrl" TEXT NOT NULL UNIQUE,
+//   "url" TEXT NOT NULL,
+//   "userId" INTEGER NOT NULL REFERENCES users(id)
+//   "visitCount" INTEGER NOT NULL DEFAULT 0  
+// );
+
+// CREATE TABLE urls (
+//   id SERIAL PRIMARY KEY, 
+//   url TEXT NOT NULL, 
+//   shortUrlId INTEGER NOT NULL REFERENCES shortUrls(id) 
+// );
+
